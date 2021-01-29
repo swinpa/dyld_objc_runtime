3b1e04173fd02425aefd1dfb3bb388c787c4442f
mac <>
2021-01-29 18:25:36 +0800

    尝试编译iOS工程设置

--- runtime/objc-runtime-new.mm
+++ runtime/objc-runtime-new.mm
@@ -39,7 +39,16 @@
  for ios
  //#include <mach/shared_region.h>
  */
+
+#ifdef  IS_iOS
+    
+#else
+
 #include <mach/shared_region.h>
+
+#endif
+
+
 
 #define newprotocol(p) ((protocol_t *)p)
 
@@ -6838,26 +6847,23 @@ classSlotForTagIndex(objc_tag_index_t tag)
 * use.
 **********************************************************************/
 
-/*
- for ios
- 
- static void
- initializeTaggedPointerObfuscator(void)
- {
-     //if (sdkIsOlderThan(10_14, 12_0, 12_0, 5_0, 3_0) ||
-         // Set the obfuscator to zero for apps linked against older SDKs,
-         // in case they're relying on the tagged pointer representation.
-         //DisableTaggedPointerObfuscation) {
-         //objc_debug_taggedpointer_obfuscator = 0;
-     //} else {
-         // Pull random data into the variable, then shift away all non-payload bits.
-         arc4random_buf(&objc_debug_taggedpointer_obfuscator,
-                        sizeof(objc_debug_taggedpointer_obfuscator));
-         objc_debug_taggedpointer_obfuscator &= ~_OBJC_TAG_MASK;
-     //}
- }
- 
- */
+#ifdef  IS_iOS
+static void
+initializeTaggedPointerObfuscator(void)
+{
+    //if (sdkIsOlderThan(10_14, 12_0, 12_0, 5_0, 3_0) ||
+        // Set the obfuscator to zero for apps linked against older SDKs,
+        // in case they're relying on the tagged pointer representation.
+        //DisableTaggedPointerObfuscation) {
+        //objc_debug_taggedpointer_obfuscator = 0;
+    //} else {
+        // Pull random data into the variable, then shift away all non-payload bits.
+        arc4random_buf(&objc_debug_taggedpointer_obfuscator,
+                       sizeof(objc_debug_taggedpointer_obfuscator));
+        objc_debug_taggedpointer_obfuscator &= ~_OBJC_TAG_MASK;
+    //}
+}
+#else
 
 static void
 initializeTaggedPointerObfuscator(void)
@@ -6874,6 +6880,10 @@ initializeTaggedPointerObfuscator(void)
         objc_debug_taggedpointer_obfuscator &= ~_OBJC_TAG_MASK;
     }
 }
+
+#endif
+
+
 
 
 /***********************************************************************
