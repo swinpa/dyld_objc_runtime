cf3ab5baca1bdb72e561615a5fd24cfeafcccbb8
prders <>
2021-08-04 00:24:22 +0800

    新增：objc_818最新版 此版本编译暂未通过 不影响就版本objc使用 新版编译待处理

---
+++ objc4-818.2/objc_ios/objc_ios.h
@@ -0,0 +1,12 @@
+//
+//  objc_ios.h
+//  objc_ios
+//
+//  Created by priders on 2021/8/3.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface objc_ios : NSObject
+
+@end
---
+++ objc4-818.2/objc_ios/objc_ios.m
@@ -0,0 +1,12 @@
+//
+//  objc_ios.m
+//  objc_ios
+//
+//  Created by priders on 2021/8/3.
+//
+
+#import "objc_ios.h"
+
+@implementation objc_ios
+
+@end
